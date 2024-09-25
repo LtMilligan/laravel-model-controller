fc774e21b4fcd77cb2aa9542075bac2a09517431
LtMilligan <gioele.miscia@gmail.com>
2024-09-25 16:23:15 +0200

    [FIX] Add PageController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">VIDEOTECA BOOLEAN</h2>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">Laravel Model Controller</h2>
+            </div>
+            <div class="col-12">
+                <ul class="list-unstyled m-0 d-flex justify-content-center">
+                    <li class="p-3"><a class="nav-link" href="#">Home</a></li>
+                    <li class="p-3"><a class="nav-link" href="#">Movies</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
